fbcfd52d9fce9f914d687b430df73048b273d613
0x7030676e31 <p0gn10wn14@gmail.com>
2022-07-16 10:59:57 +0200

    "debug" console.log remove

--- src/modules/memeScraper.ts
+++ src/modules/memeScraper.ts
@@ -25,8 +25,6 @@ module.exports = {
     const linkVideos: string[] = d.content ? d.content.match(vid_reg) || [] : [];
     const attachments: any[] = d.attachments ? d.attachments : [];
 
-    console.log(linkVideos);
-
     if (linkVideos.length === 0 && attachments.length === 0)
       return
 
